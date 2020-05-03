a7fe2757c8de751e0f48fa7709bc3624c8251da5
fidelisakilan <fidelisakilan@gmail.com>
2020-05-03 15:42:20 +0530

    setup done

---
+++ android/app/src/main/kotlin/com/example/dev_fest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dev_fest
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
